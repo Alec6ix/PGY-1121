8dea5bf428827b370ec5c792b3ac3dfc2ff2cc43
Alexis <118229235+Alec6ix@users.noreply.github.com>
2023-05-25 18:04:29 -0400

    Add files via upload

--- CLASE_12/Lista.py
+++ CLASE_12/Lista.py
@@ -1,5 +1,2 @@
 lista = []
 print(type(lista))
-
-lista2 = ['Alexis Escobar', 20, 6.5, True,'S']
-print(lista2)
---
+++ CLASE_12/Lista2.py
@@ -0,0 +1,13 @@
+lista = ['Alexis Escobar', 51, 6.5, True, 'S']
+print(lista)
+print("---------------")
+'''
+print(lista[0])
+print(lista[1])
+print(lista[2])
+print(lista[3])
+print(lista[4])
+'''
+print("---------------")
+for n in lista:
+    print(n)
